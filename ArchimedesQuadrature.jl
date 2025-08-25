7b67a5f705c06e102ebd4052b1d923c725f53c1f
inigorrix <inigorrix@gmail.com>
2025-08-25 19:42:15 +0200

    implemented recursive method for 1D

--- ArchimedesQuadrature.jl
+++ ArchimedesQuadrature.jl
@@ -72,12 +72,12 @@ end
 						   default=["aproximation", "function"])
 
 # ╔═╡ 31a3d986-1f3f-4bfc-98dc-17c84f574e4e
+# Iterative method
 function calc_1d_aprox_iter(func, level)
 	n_1d = 2^level+1
-	x_1d = range(0,1,length=n_1d)
 	aprox = zeros(plot_len)
 	surps = ones(n_1d)
-	weights = ones(n_1d) * .5
+	quad = (func(0)+func(1))*.5
 	
 	for lev in 0:level
 		n_lev = 2^lev
@@ -86,18 +86,51 @@ function calc_1d_aprox_iter(func, level)
 			id = Int(i*hn_lev*(n_1d-1)+1)
 			surps[id] = func(i*hn_lev)
 			if lev>0
-				weights[id] = hn_lev
-				surps[id] -= (func((i-1)*hn_lev) + func((i+1)*hn_lev))/2
+				surps[id] -= (func((i-1)*hn_lev) + func((i+1)*hn_lev)) * .5
+				quad += surps[id] * hn_lev
 			end
 			aprox += hat_func.(x_plot, i*hn_lev, hn_lev)*surps[id]
 		end
 	end
-	return aprox, surps, sum(surps .* weights)
+	return aprox, surps, quad
+end
+
+# ╔═╡ b04a5a06-c1d5-478e-9a92-55dc5a9750ab
+# Recursive method
+function calc_1d_level(func, max_level, lev, aprox, surps, quad)
+	if lev > max_level
+		return quad
+	end
+	
+	n_lev = 2^lev
+	hn_lev = 1/n_lev
+	for i in min(1,lev):min(2,lev+1):n_lev
+		id = Int(i*hn_lev*(2^max_level)+1)
+		surps[id] = func(i*hn_lev)
+		if lev>0
+			surps[id] -= (func((i-1)*hn_lev) + func((i+1)*hn_lev)) * .5
+			quad += surps[id] * hn_lev
+		end
+		aprox .+= hat_func.(x_plot, i*hn_lev, hn_lev)*surps[id]
+	end
+	return calc_1d_level(func, max_level, lev+1, aprox, surps, quad)
+end
+
+# ╔═╡ fdb10ff2-82f4-4431-8d3e-679b0800e5e3
+# Recursive method
+function calc_1d_aprox_rec(func, max_level)
+	n_1d = 2^max_level+1
+	aprox = zeros(plot_len)
+	surps = ones(2^max_level+1)
+	quad = (func(0)+func(1))*.5
+
+	quad = calc_1d_level(func, max_level, 0, aprox, surps, quad)
+	return aprox, surps, quad
 end
 
 # ╔═╡ 554f531a-6b27-401f-a58e-4a7d6f5817d7
 begin
-	aprox_1d, surps_1d, quad_1d = calc_1d_aprox_iter(func_1d, level_1d)
+	aprox_1d, surps_1d, quad_1d = calc_1d_aprox_rec(func_1d, level_1d)
 	fig_1d_aprox = Figure()
 	ax_1d_aprox = Axis(fig_1d_aprox[1, 1])
 	if "aproximation" in to_plot
@@ -1825,9 +1858,11 @@ version = "1.9.2+0"
 # ╟─ca6ce552-2ac0-44d3-98bb-e0069ea0908e
 # ╟─b23ad289-e6ef-468e-80a9-66c2a44ce38f
 # ╟─07fb8f13-00be-42e1-aa5f-aad6dd80e6fb
-# ╠═d6e321f7-dbe0-47e3-bada-795243a97da4
+# ╟─d6e321f7-dbe0-47e3-bada-795243a97da4
 # ╟─74cd64ff-5ace-4fcf-bac1-d3e4e656aa14
 # ╟─31a3d986-1f3f-4bfc-98dc-17c84f574e4e
+# ╟─fdb10ff2-82f4-4431-8d3e-679b0800e5e3
+# ╟─b04a5a06-c1d5-478e-9a92-55dc5a9750ab
 # ╟─554f531a-6b27-401f-a58e-4a7d6f5817d7
 # ╟─8acaf1a0-ee2e-427e-a448-2a064f8a0876
 # ╟─cf87a49e-11d8-4aa9-87d5-7acf32bac9b7
